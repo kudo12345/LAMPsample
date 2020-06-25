6fd5f95ad7c24bd812d4d1331cf55523cbef5e67
kudo12345 <a.kudo.sys19@morijyobi.ac.jp>
2020-06-25 11:38:41 +0900

    aaa

--- index.php
+++ index.php
@@ -128,8 +128,8 @@ try {
                                 <td><?php echo "$value[age]" ?></td>
                                 <td>
                                     <from action="./delete.php" method="$_GET">
-                                        <input type="text" class="d-none" name="id" value="<?php echo "$value[id]" ?>">
-                                        <button type="submit" class="btn btn-danger">delete</button>
+                                    <input type="text" values="<?php echo "$value[id]" ?>" class="d-none" name="id">
+                                        <button type="submit" class="btn btn-denger">delete</button>
                                     </from>
                                 </td>
                             </tr>
